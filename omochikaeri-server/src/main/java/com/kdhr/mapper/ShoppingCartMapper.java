d2c45d066792c722476fca03bbe4aeaa11fc6a33
downwarjers <karta1000123@gmail.com>
2024-02-25 20:58:42 +0800

    增加訂單功能

--- omochikaeri-server/src/main/java/com/kdhr/mapper/ShoppingCartMapper.java
+++ omochikaeri-server/src/main/java/com/kdhr/mapper/ShoppingCartMapper.java
@@ -46,4 +46,9 @@ public interface ShoppingCartMapper {
     @Delete("delete from shopping_cart where id = #{id}")
     void deleteById(Long id);
 
+    /**
+     * 批量插入
+     * @param shoppingCartList
+     */
+    void insertBatch(List<ShoppingCart> shoppingCartList);
 }
